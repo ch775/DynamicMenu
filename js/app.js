951cd0614e902e54457ccd1ad1a48b4b100c26be
Christina <hbts93@gmail.com>
2020-02-04 17:23:32 -0700

    Fixed navigation

--- js/app.js
+++ js/app.js
@@ -8,10 +8,11 @@ const navigation = document.querySelector("#navigation");
 //This loops over the number of sections and ads a div with section name for each.
 for (var i=0; i < sections.length; i++) {
     const newMenuItemDiv = document.createElement ('div'); //This creates a new div for menu
-    newMenuItemDiv.classList.add("menuItem");
+    newMenuItemDiv.classList.add("menuItem2");
     const menuItemText = document.querySelectorAll(".menuItem")[i].firstChild.nodeValue; //This grabs the inner text from the sections h2 tag.
-    const menuItem = newMenuItemDiv.innerHTML += menuItemText; //Adds text to new div
-    navigation.insertAdjacentHTML('beforeend', menuItem); //This adds menu item to nav
+    newMenuItemDiv.innerHTML = menuItemText;
+    //newMenuItemDiv.innerHTML(menuItemText); //Adds text to new div
+    navigation.append(newMenuItemDiv); //This adds menu item to nav
 };
 
 
